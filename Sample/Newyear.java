54241bea924a9b19da3ee6e7bdfb4c8638f45647
T--Yagoo <thiyagarasan6380@gmail.com>
2026-07-10 17:15:22 +0530

    Add Newyear class with countdown and greeting

---
+++ Sample/Newyear.java
@@ -0,0 +1,11 @@
+class Newyear{
+    public static void main(String[] args) {
+        int i;
+        for (i=50;i<60;i++){
+            System.out.println("11:"+i);
+            
+        }
+        System.out.println("Happy Newyear");
+        
+    }
+}
